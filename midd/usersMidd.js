ff00c460cafbd711875a6f4f6a6b133dcde9d8ed
itai1989 <itait1989@gmail.com>
2025-02-26 20:36:19 +0200

    Added user registration functionality via POST request

--- midd/usersMidd.js
+++ midd/usersMidd.js
@@ -1,9 +1,33 @@
 const pool = require("../db");
 
-async function name(req, res, next) {
-  const [rows] = await pool.query("SELECT * FROM users");
-  res.users = rows;
-  next();
+async function getUsers(req, res, next) {
+    try {
+        const [rows] = await pool.query("SELECT * FROM users");
+        res.users = rows;
+        next();
+    } catch (error) {
+        res.status(500).json({ message: "שגיאת שרת", error: error.message });
+    }
 }
 
-module.exports = name;
+async function addUser(req, res, next) {
+    const name = req.body.name;
+
+    try {
+        const [result] = await pool.query(
+            "INSERT INTO users (name) VALUES (?)",
+            [name]
+        );
+
+        res.newUser = {
+            message: "המשתמש נוסף בהצלחה",
+            user_id: result.insertId
+        };
+
+        next();
+    } catch (error) {
+        res.status(500).json({ message: "שגיאת שרת", error: error.message });
+    }
+}
+
+module.exports = { getUsers, addUser };
--- routs/users.js
+++ routs/users.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routs = express.Router();
 const usersMidd = require('../midd/usersMidd');
 
-routs.get('/',usersMidd,(req,res)=>{
+routs.get('/', usersMidd.getUsers, (req, res) => {
     res.status(200).json(res.users);
-})
+});
 
-
+routs.post('/', usersMidd.addUser, (req, res) => {
+    res.status(201).json(res.newUser);
+});
 
 module.exports = routs;
